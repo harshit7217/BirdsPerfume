f9b0cc0534a0a90071cf43b2f5077c1b771e446a
Prasang41 <harshitchaudhary568@gmail.com>
2025-08-29 07:48:09 +0530

    Navbar Responsvie

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from './components/Navbar/Navbar'
 import { Outlet } from 'react-router-dom'
 import Footer from './components/Footer/Footer'
 
 function Layout() {
+  const [width, setWidth] = useState<number>(window.innerWidth);
+  useEffect(() => {
+    const handleResize = () => setWidth(window.innerWidth);
+  window.addEventListener('resize', handleResize);
+
+  // Cleanup on unmount
+  return () => window.removeEventListener('resize', handleResize);
+  }, [width])
   return (
     <div>
-        <Navbar />
+        <Navbar size={width}/>
         <Outlet/>
         <Footer/>
     </div>
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,25 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { CiMenuBurger } from 'react-icons/ci'
 import { FaHome } from 'react-icons/fa'
 import { MdOutlineProductionQuantityLimits } from 'react-icons/md'
 import { Link } from 'react-router-dom'
 
+type Props = {
+    size: number;
+}
 
-function Navbar() {
+function Navbar(props: Props) {
+    const [menu, setMenu] = useState<boolean>(false);
+
+    const handleNavbar = () => {
+        setMenu(!menu);
+    }
+
     return (
-        <div className='bg-gray-50 flex justify-between items-center overflow-hidden'>
-            <div className='pl-[10%] h-30 flex items-center'>
-                <img src="./Logo.png" alt="" className='w-70 h-auto'/>
-            </div>
-            <div className='flex gap-20 pr-[10%]'>
+        <div>
+            {props.size > 720 ? (
+                <div className='bg-gray-50 flex justify-between items-center overflow-hidden'>
+                    <div className='pl-[10%] h-30 flex items-center'>
+                        <img src="./Logo.png" alt="" className='w-70 h-auto' />
+                    </div>
+                    <div className='flex gap-20 pr-[10%]'>
 
-                <Link to='' className='flex items-center gap-1 text-3xl font-serif text-gray-500'>
-                    <FaHome /> Home
-                </Link>
-                <Link to='products' className='flex items-center gap-1 text-3xl font-serif text-gray-500'>
-                    <MdOutlineProductionQuantityLimits/> Products
-                </Link>
-            </div>
-            
+                        <Link to='' className='flex items-center gap-1 text-3xl font-serif text-gray-500'>
+                            <FaHome /> Home
+                        </Link>
+                        <Link to='products' className='flex items-center gap-1 text-3xl font-serif text-gray-500'>
+                            <MdOutlineProductionQuantityLimits /> Products
+                        </Link>
+                    </div>
+                </div>
+            ) : (
+                <div className='flex justify-between items-center pr-10'>
+                    <div className='pl-[10%] h-15 flex items-center'>
+                        <img src="./Logo.png" alt="" className='w-40 h-auto' />
+                    </div>
+                    <div>
+                        <button className='cursor-pointer' onClick={handleNavbar} ><CiMenuBurger className='w-7 h-auto' /></button>
+                    </div>
+                </div>
+            )}
+            {menu === true ? (
+                <div data-aos='fade-right' className='flex flex-col justify-center items-start pl-20 '>
+                    <Link to='' className='flex items-center gap-10 text-3xl font-serif text-gray-500'>
+                        <FaHome /> Home
+                    </Link>
+                    <Link to='products' className='flex items-center gap-5 text-3xl font-serif text-gray-500'>
+                        <MdOutlineProductionQuantityLimits /> Products
+                    </Link>
+                </div>
+            ) : null}
         </div>
     )
 }
